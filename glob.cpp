80b5ca3f2345db760fe0c5cfecbc06a42f3d42d1
Herman Lundkvist <herlu184@student.liu.se>
2015-02-26 11:18:44 +0100

    Added a debug function for outputting hash512's value to hex. Hash512 was tested and worked with the input string "asdfghjkl".

--- glob.cpp
+++ glob.cpp
@@ -1,8 +1,25 @@
 #include "glob.h"
 #include "cryptopp/sha.h"
+#include "cryptopp/hex.h"
+#include <sstream>
+
+const unsigned int HASH_SIZE = CryptoPP::SHA512::DIGESTSIZE;
 
 hash_t hash512(const string& value){
-    hash_t hashed_val(new unsigned char[CryptoPP::SHA512::DIGESTSIZE]);
+    hash_t hashed_val(new unsigned char[HASH_SIZE]);
     CryptoPP::SHA512().CalculateDigest(hashed_val.get(), (byte*) value.data(), value.size());
     return hashed_val;
+}
+
+/*
+ * Uses crypto++' hexencoder.
+ */
+void debug_hash512(const hash_t& hash){
+    CryptoPP::HexEncoder encoder;
+    std::string output;
+    encoder.Attach( new CryptoPP::StringSink( output ) );
+    encoder.Put( hash.get(), HASH_SIZE );
+    encoder.MessageEnd();
+
+    debug("Hash value is: %s\n", output.c_str());
 }
--- glob.h
+++ glob.h
@@ -61,8 +61,11 @@ void debug(const std::string& fmt, const Ts&...args){
 typedef std::unique_ptr<unsigned char[]> hash_t;
 /* Hash function that uses SHA-3, implemented using libcrypto++.
  * The function allocates a 64 byte on the heap using unique_ptr
- * the result is moved into the return value
+ * the result is moved into the return value.
 */
 hash_t hash512(const string& value);
+
+/* Prints a hash_t using the debug function.*/
+void debug_hash512(const hash_t&);
 
 #endif // GLOB_H
