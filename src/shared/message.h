32820b42ebf4d9370411754017ee3c1a7149890f
Herman Lundkvist <herlu184@student.liu.se>
2015-03-23 21:57:45 +0100

    Added enums and function defintions for spreading of meta heads

--- src/shared/message.h
+++ src/shared/message.h
@@ -28,7 +28,7 @@ public:
     T_CHUNK_REQ,
     T_UI_TEXT_COMMAND
     T_META_REQ, //sent by a client requesting meta heads
-    T_META_LIST, //message contains a list of metaheads
+    T_META_LIST //message contains a list of metaheads
   }; // message types
   enum msg_keys : Key_type_t {
     K_BODY,
