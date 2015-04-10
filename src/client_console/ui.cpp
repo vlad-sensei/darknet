9d5d89dbb06b71beb09411fe14e10c25264db545
Anthon <antjo383@student.liu.se>
2015-04-10 13:37:19 +0200

    fungerande terminal utan readline paket, behövar mergas för hand med ui_shared

--- src/client_console/ui.cpp
+++ src/client_console/ui.cpp
@@ -1,4 +1,4 @@
-#include "ui.h"
+﻿#include "ui.h"
 
 #include <thread>
 #include <mutex>
@@ -32,8 +32,8 @@ void UI::echo(const string &msg){
 
 // ~~~~~~~~~~~~ text input ~~~~~~~~~~~~
 
-string find_match(string input);
-void print_terminal_content(vector<string> terminal_content);
+string find_match(string input); //autofill funktion
+void print_terminal_content(vector<string> terminal_content, int content_index);
 
 char** cmd_list;
 vector<char*> command_keys;
@@ -44,20 +44,24 @@ void UI::get_text_input(){
 
     init_readline();
 
-    vector<string> terminal_content;
-    vector<string> old_commands;
+    vector<string> terminal_content; //all the terminal content to be printed
+    vector<string> old_commands; //all old used commands
+
     //initiate curse window
     initscr();
     noecho();    
-    cbreak();
-    keypad(stdscr, TRUE);
+    cbreak();  //disables input buffering
+    keypad(stdscr, TRUE); //allow special keys like arrow up etc
 
-    string line("> ");
-    int c,x,y,max_x,max_y,history_index, content_index = 0;
+    string line("");
+    size_t c,x,y,max_x,max_y,history_index, content_index = 0;
 
-    terminal_content.push_back(" ~~~ welcome to darknet ~~~\n");
+    terminal_content.push_back(" ~~~ welcome to darknet ~~~ ");
     terminal_content.push_back("> ");
+    terminal_content.push_back("");
+
     printw(terminal_content[0].c_str(),0);
+    move(1,0);
     printw(terminal_content[1].c_str(),1);
 
     while(1){
@@ -68,56 +72,59 @@ void UI::get_text_input(){
         getmaxyx(stdscr,max_y,max_x);
 
         line.clear();
-        line = terminal_content[terminal_content.size()-1];
+        line = terminal_content[1];
 
         switch(c){        
         case KEY_BACKSPACE:
             if(x > 2){
-                terminal_content[terminal_content.size()-1].pop_back();
+                terminal_content[1].pop_back();
                 move(y,x-1);
                 delch();
             }
             break;
         case '\n'://ENTER            
-            if(line.size()>0) old_commands.insert(old_commands.begin(),terminal_content[terminal_content.size()-1]);
-            terminal_content[terminal_content.size()-1] += '\n';
-            terminal_content.push_back("> ");
+            if(line.size()>0) old_commands.insert(old_commands.begin(),terminal_content[1]);
+            terminal_content.insert(terminal_content.begin()+1,"> ");
             break;
         case KEY_UP:            
             if(history_index == old_commands.size()){
-                terminal_content[terminal_content.size()-1] = "> ";
+                terminal_content[1] = "> ";
                 history_index = 0;
             }
             else{
-                terminal_content[terminal_content.size()-1] = old_commands[history_index];
+                terminal_content[1] = old_commands[history_index];
                 history_index++;
             }           
             break;
         case KEY_DOWN:
             if(history_index > 0 ){
                 history_index--;
-                terminal_content[terminal_content.size()-1] = old_commands[history_index];
+                terminal_content[1] = old_commands[history_index];
             }
             break;
-        case KEY_PPAGE://page up
+
+        case KEY_PPAGE://page up           
+            if(content_index > 0)content_index--;
             break;
         case KEY_NPAGE://page down
-            break;
-        case '\t':            
-            terminal_content[terminal_content.size()-1] = find_match(terminal_content[terminal_content.size()-1]);
+            if(max_y < terminal_content.size() && content_index < terminal_content.size() - max_y)content_index++;
             break;
 
-        case KEY_RESIZE:
+        case '\t':            
+            terminal_content[1] = find_match(terminal_content[1]);
+            break;
+
+        case KEY_RESIZE: //prevents resize character from being printed in terminal
             break;
         default:
-            terminal_content[terminal_content.size()-1] += c;
+            terminal_content[1] += c;
         }
 
-        //reset the history index if c not arrow up
+        //reset the history index if c not arrow up/down
         if(c != KEY_UP && c != KEY_DOWN)history_index = 0;
 
         //clear print area and print new text
-        print_terminal_content(terminal_content);
+        print_terminal_content(terminal_content, content_index);
 
         refresh();
 
@@ -146,8 +153,8 @@ string find_match(string input){
 
 
 
-void print_terminal_content(vector<string>terminal_content){
-    int c,x,y,max_x,max_y =0;
+void print_terminal_content(vector<string>terminal_content, int content_index){
+    size_t x,y,max_x,max_y =0;
     string line = "";
 
     getyx(stdscr,y,x);
@@ -155,49 +162,41 @@ void print_terminal_content(vector<string>terminal_content){
 
     erase();        //clear entire terminal
 
+    size_t index = 0;
+    size_t i = 0;
+    bool scroll_set = FALSE;
 
-    int index = 0;
-    int i = 0;
     while(index < max_y){
 
-        if(i >= terminal_content.size())break;
+        if(i >= terminal_content.size()-1)break;
 
-        else if(line.size() < 1) line = terminal_content[i];  // 1 line per content row
+        if(i > 1 && !scroll_set){
+            i += content_index;
+            scroll_set = TRUE;
+        }
+        if(line.size() < 1) line = terminal_content[i];  // 1 line per content row
 
         else if(line.size() < max_x){
             printw(line.c_str(),index);
             line.clear();
             i++;
             index++;
+            move(index,0);
         }
-
         else if(line.size() >= max_x){
             string temp = line.substr(0,max_x);
             printw(temp.c_str(),index);
-            //line = "ABCDEFGH";
-            //line = line.substr(3);
             line = line.substr(max_x);
             if(line.size() < 1)i++;
             index++;
+            move(index,0);
         }
 
-
-
     }
-
-
-    /*
-    int i = 0;
-
-    for(i = 0;i<max_y;i++){
-        if(i>=terminal_content.size())break;
-        else{
-            move(i,0);
-            printw(terminal_content[i+content_index].c_str(),i);
-        }
-    }
-    */
-    //printw("mmmmmm",2);
+    // move cursor to correct position
+    int temp1 = 1+terminal_content[1].size()/max_x;
+    int temp2 = terminal_content[1].size() % max_x;
+    move(temp1,temp2);
     refresh();
 }
 
