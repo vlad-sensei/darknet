b0bc4ec27b0166d8835eaeeb28dae51c958078b7
Vladimir Valyukh <vladimir.sensei@gmail.com>
2015-04-26 16:52:16 +0200

    blah

--- src/client_console/ui.h
+++ src/client_console/ui.h
@@ -26,7 +26,7 @@ private:
 
 #ifdef NCURSES
   string find_match(const string& input); //autofill funktion
-  void print_terminal_content(vector<string> terminal_content, int content_index);
+  void print_terminal_content(const vector<string>& terminal_content, int content_index);
   void init_window();
 #endif
 
