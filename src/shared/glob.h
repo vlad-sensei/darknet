de0203fea39cc3400e4cebc282dd4a1edd8e11cc
Andreas Norrstig <andno037@student.liu.se>
2015-05-16 23:13:16 +0200

    testa lengre delay pq ai

--- src/shared/glob.h
+++ src/shared/glob.h
@@ -107,7 +107,7 @@ typedef uint32_t ip_t;
 #define DEFAULT_ARENA_PATH "/tmp/arena"
 #define DEFAULT_ARENA_SLOT_NUM 200
 #define DEAFULT_PEER_REQ_COUNT 10
-#define DEFAULT_AI_SLEEP 5
+#define DEFAULT_AI_SLEEP 15
 #define DEFAULT_WAIT_TO_AGGRESIV 10 //in seconds
 
 
